d5149503b0606fb9a7527819d17cacfcb917dbbb
SHIRO <58366293+CryptoEvil-Dev@users.noreply.github.com>
2026-03-30 21:52:33 +0300

    Проведён полный рефакторинг кода. Исправлена ошибка в названии библиотеки (Было: liblibnet, Стало: libnet). Во все заголовочные файлы были добавлены структуры данных.

---
+++ tun/tun.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "tun/include/dev.hpp"
--- types/types.hpp
+++ types/types.hpp
@@ -2,3 +2,5 @@
 
 #include "types/include/ipv4.hpp"
 #include "types/include/ipv6.hpp"
+#include "types/include/ipv4net.hpp"
+#include "types/include/uint128.hpp"
